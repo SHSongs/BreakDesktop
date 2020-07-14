22101fcf2dae70ba60b405016f684cbfa40584ac
Song <217.Songs.SH@gmail.com>
2020-07-14 17:53:25 +0900

    login join data

--- BreakDesktopClient/BreakDesktopClient/login.cs
+++ BreakDesktopClient/BreakDesktopClient/login.cs
@@ -12,9 +12,46 @@ namespace BreakDesktopClient
 {
     public partial class login : Form
     {
+
+        
         public login()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+  
+
+        private void Join_Click(object sender, EventArgs e)
+        {
+            String id = textBox1.Text;
+            String pass = textBox2.Text;
+            String name = textBox3.Text;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String id = textBox1.Text;
+            String pass = textBox2.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
